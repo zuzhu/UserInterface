3cd6752eb06bdab7c6def79ceda3b0ab8fca58c7
Zutao Zhu <zuzhu@syr.edu>
2009-10-11 20:22:04 -0400

    User Interface first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+partial class frmMain
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+        this.label1 = new System.Windows.Forms.Label();
+        this.label2 = new System.Windows.Forms.Label();
+        this.button1 = new System.Windows.Forms.Button();
+        this.button2 = new System.Windows.Forms.Button();
+        this.rbtSourceCode = new System.Windows.Forms.RichTextBox();
+        this.button3 = new System.Windows.Forms.Button();
+        this.SuspendLayout();
+        // 
+        // richTextBox2
+        // 
+        this.richTextBox2.Location = new System.Drawing.Point(279, 46);
+        this.richTextBox2.Name = "richTextBox2";
+        this.richTextBox2.Size = new System.Drawing.Size(249, 221);
+        this.richTextBox2.TabIndex = 1;
+        this.richTextBox2.Text = "";
+        // 
+        // label1
+        // 
+        this.label1.AutoSize = true;
+        this.label1.Location = new System.Drawing.Point(14, 24);
+        this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size(101, 13);
+        this.label1.TabIndex = 2;
+        this.label1.Text = "SPINA source code";
+        // 
+        // label2
+        // 
+        this.label2.AutoSize = true;
+        this.label2.Location = new System.Drawing.Point(282, 24);
+        this.label2.Name = "label2";
+        this.label2.Size = new System.Drawing.Size(78, 13);
+        this.label2.TabIndex = 3;
+        this.label2.Text = "OutputWindow";
+        // 
+        // button1
+        // 
+        this.button1.Location = new System.Drawing.Point(27, 289);
+        this.button1.Name = "button1";
+        this.button1.Size = new System.Drawing.Size(75, 23);
+        this.button1.TabIndex = 4;
+        this.button1.Text = "&Load";
+        this.button1.UseVisualStyleBackColor = true;
+        this.button1.Click += new System.EventHandler(this.button1_Click);
+        // 
+        // button2
+        // 
+        this.button2.Location = new System.Drawing.Point(168, 289);
+        this.button2.Name = "button2";
+        this.button2.Size = new System.Drawing.Size(75, 23);
+        this.button2.TabIndex = 5;
+        this.button2.Text = "&Save";
+        this.button2.UseVisualStyleBackColor = true;
+        this.button2.Click += new System.EventHandler(this.button2_Click);
+        // 
+        // rbtSourceCode
+        // 
+        this.rbtSourceCode.Location = new System.Drawing.Point(17, 45);
+        this.rbtSourceCode.Name = "rbtSourceCode";
+        this.rbtSourceCode.Size = new System.Drawing.Size(235, 223);
+        this.rbtSourceCode.TabIndex = 0;
+        this.rbtSourceCode.Text = "a=[1,2,3;4,5,6];\nb=[2;4;6];\nc=a*b;\nprint c;\nprint a;\nprint b;";
+        // 
+        // button3
+        // 
+        this.button3.Location = new System.Drawing.Point(370, 289);
+        this.button3.Name = "button3";
+        this.button3.Size = new System.Drawing.Size(75, 23);
+        this.button3.TabIndex = 6;
+        this.button3.Text = "&Run";
+        this.button3.UseVisualStyleBackColor = true;
+        this.button3.Click += new System.EventHandler(this.button3_Click);
+        // 
+        // frmMain
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(540, 359);
+        this.Controls.Add(this.button3);
+        this.Controls.Add(this.button2);
+        this.Controls.Add(this.button1);
+        this.Controls.Add(this.label2);
+        this.Controls.Add(this.label1);
+        this.Controls.Add(this.richTextBox2);
+        this.Controls.Add(this.rbtSourceCode);
+        this.MaximizeBox = false;
+        this.Name = "frmMain";
+        this.Text = "SPINA";
+        this.ResumeLayout(false);
+        this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.RichTextBox richTextBox2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.RichTextBox rbtSourceCode;
+    private System.Windows.Forms.Button button3;
+}
---
+++ frmMain.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+    //public delegate void SampleDelegate(string message);
+
+public partial class frmMain : Form
+{
+
+    StringBuilder sb;
+    string content;
+    StringBuilder sbOutput;
+    bool readflag = false;
+    bool endflag = false;
+
+    public frmMain()
+    {
+        InitializeComponent();
+        sb = new StringBuilder();
+        sbOutput = new StringBuilder();
+        //readflag = false;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Title = "Open SPINA File";
+        openFileDialog.Filter = "SPINA Files(*.spi)|*.spi";
+        openFileDialog.InitialDirectory = @"C:\";
+        openFileDialog.CheckFileExists = true;
+        openFileDialog.CheckPathExists = true;
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            //MessageBox.Show(openFileDialog.FileName.ToString());
+            ShowSourceCode(openFileDialog.FileName);
+        }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.Title = "Save SPINA File";
+        saveFileDialog.Filter = "SPINA Files(*.spi)|*.spi";
+        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            SaveSourceCode(saveFileDialog.FileName);
+        }
+    }
+
+    private void ShowSourceCode(string fileName)
+    {
+        sb.Remove(0, sb.Length);
+        using (StreamReader sr = new StreamReader(fileName))
+        {
+            String line;
+            //StringBuilder sb = new StringBuilder();
+            while ((line = sr.ReadLine()) != null)
+            {
+                sb.AppendLine(line);
+            }
+            rbtSourceCode.Text = sb.ToString();
+        }
+    }
+
+    private void SaveSourceCode(string fileName)
+    {
+        using (StreamWriter sw = new StreamWriter(fileName))
+        {
+            sw.Write(rbtSourceCode.Text);
+            sw.Close();
+        }
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        endflag = false;
+        sbOutput.Remove(0, sbOutput.Length);
+        richTextBox2.Clear();
+        SampleDelegate d1 = WriteOutput;
+        consoleProgram theprogram = new consoleProgram();
+        content = rbtSourceCode.Text;
+        //content.Replace('\n', ' ');
+        theprogram.setText(content);
+        theprogram.setDelegate(d1);
+        Thread oThread = new Thread(new ThreadStart(theprogram.VisitLine));
+        oThread.Start();
+        //Thread pThread = new Thread(new ThreadStart(ReadFromBuffer));
+        //pThread.Start();
+        //theprogram.VisitLine();
+        while (!endflag)
+        {
+            ReadFromBuffer();
+        }
+        oThread.Abort();
+    }
+
+    private void WriteOutput(string s)
+    {
+        lock (this)
+        {
+            if (readflag)
+            {
+                try
+                {
+                    Monitor.Wait(this);   // Wait for the Monitor.Pulse in
+                    // ReadFromCell
+                }
+                catch (SynchronizationLockException e)
+                {
+                    Console.WriteLine(e);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+            sbOutput.Append(s);
+            readflag = true;
+            Monitor.Pulse(this);
+        }
+    }
+
+    private void ReadFromBuffer()
+    {
+        lock (this)
+        {
+            if (!readflag)
+            {
+                try
+                {
+                    Monitor.Wait(this);
+                }
+                catch (SynchronizationLockException e)
+                {
+                    Console.WriteLine(e);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            //Console.WriteLine("Consume: {0}", cellContents);
+            richTextBox2.AppendText(sbOutput.ToString());
+            if (sbOutput.ToString().Equals("END"))
+                endflag = true;
+            sbOutput.Remove(0, sbOutput.Length);
+            readflag = false;    // Reset the state flag to say consuming
+            // is done.
+            Monitor.Pulse(this);   // Pulse tells Cell.WriteToCell that
+            // Cell.ReadFromCell is done.
+        }
+    }
+
+}
+
